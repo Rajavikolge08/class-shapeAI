9a91067373421e8e9bf069a4f94c6df3a5f7895e
Rajavikolge08 <kolgerajavi@gmail.com>
2022-01-18 22:45:50 +0530

    javascript basics

--- app.js
+++ app.js
@@ -1,43 +1,82 @@
-console.log('hi')
-// /*hfdjfhdj*/
-// let age = 23
-// console.log(age)
-// //string
-// let brand = 'dfdfdfd'
-// console.log(brand)
-// //boolean
-// let istrue = true
-// console.log(istrue)
 
-// console.log(typeof 19)
-// console.log(typeof 'string')
-// console.log(typeof true)
-// console.log(typeof null)
-// console.log(typeof undefined)
+let array = ["GO to the gym", "Eat Breakfast", "Study to the exam"]
 
-// // assignment operator
-// const age = 80 
-// let age = 12
-// console.log(age)
-// //comparison operator
-// let a = 10
-// let b = 67
-// console.log(a < b)
-// //
-// let a = 10
-// let b = '10'
-// console.log(a == b)
+const body = document.querySelector('body')
+
+array.forEach((e, i) => {
+    const ele = document.createElement('div')
+    const text = document.createTextNode(e)
+    ele.append(text)
+    console.log(ele)
+    body.appendChild(ele)
 
 
-// //arithmatic operator
-// console.log('true && true',true && true)
-// console.log('true && false',true && false)
-// console.log('true && true',true && true)
-// console.log('true && true',true && true)
+})
 
-//
-let brand = "mercedes"
-console.log(brand.toUpperCase())
-console.log(brand.big)
-const something = new
 
+let elements = document.getElementsByClassName('temp')
+let elements = document.querySelectorAll('temp')
+
+let arr = [1, 2, 3, 4, 5]
+
+console.log(elements)
+console.log(arr)
+
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = `Element ${i}`
+
+}
+
+elements.forEach((ele, i) => {
+    ele.innerHTML = Element ${i} 
+});
+const body = document.querySelector('body')
+console.log(body.children)
+{ const node = document.createElement("div")
+const textNode = document.createTextNode('This is dynamic element')
+body.appendChild(node)
+console.log(ele)
+console.log(textNode)
+body.appendChild(ele)
+}
+
+{
+const tasks = ['go to the gym','take breakfast','take medicine','eat','sleep']
+
+const ol = document.createElement('ol')
+
+tasks.forEach(task => {
+    const li = document.createElement("li")
+    const textNode = document.createTextNode(task)
+    li.appendChild(textNode)
+    
+    ol.appendChild(li)
+})
+console.log(ol)
+body.appendChild(ol)
+}
+
+const button = document.querySelector('button')
+const 
+
+button.addEventListener('click', eventHandler)
+//click button
+
+const button = document.querySelector('button')
+
+const bmw = (e) => {
+    const ele = e.target
+    ele.style = "background:red"
+}
+
+button.addEventListener('click', bmw)
+
+button.addEventListener('dblclick', e => {
+    e.target.style = "background:orange"
+})
+
+button.addEventListener('mouseover', e => {
+    e.target.style = "background:pink"
+
+})
